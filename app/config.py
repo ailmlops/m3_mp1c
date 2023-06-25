46913a1b45157332021371674bc07a5e43c67f93
ajaytevatia <ajaytevatia@gmail.com>
2023-06-25 02:14:19 -0400

    test text added

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Bike Rental Prediction"
+    PROJECT_NAME: str = "Bike Rental Prediction test"
 
     class Config:
         case_sensitive = True
